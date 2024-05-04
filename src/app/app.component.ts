7d6940d0999ab79f2f2f01e75f3e149fe5429f10
Modeste ASSIONGBON <mo.assiongbon@gmail.com>
2024-05-04 10:46:30 +0200

    Add some element to speed the practices later: - UI items (form, table, title, etc.) - Fake data for local server DB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ng17_clean';
+  title = 'todo-list';
 }
---
+++ src/app/contact.model.ts
@@ -0,0 +1,10 @@
+/* 
+TODO: Define Contact interface to match this object
+{
+    "id": "de8bcf76-e2d6-4930-8f91-73432a3ca2a1",
+    "lastName": "A.",
+    "firstName": "Modeste",
+    "Phone number": "+22570000000"
+} 
+*/
+export interface Contact {}
